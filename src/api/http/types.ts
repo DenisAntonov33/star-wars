8aae45cdcfee334f8f9e6161a18770ed0c842c4f
DenisAntonov33 <danchic95@gmail.com>
2025-02-17 19:31:49 +0400

    Revert "Feature 1"

--- src/api/http/types.ts
+++
@@ -1,6 +0,0 @@
-export interface IListResponse<Item> {
-    count: number;
-    next: string | null;
-    prev: string | null;
-    results: Item[];
-}
--- src/app/characters/components/SkeletonList.tsx
+++
@@ -1,9 +0,0 @@
-import {CharacterSkeleton} from "@/app/characters/components/CharacterSkeleton";
-
-const items = new Array(10).fill(undefined);
-
-export const SkeletonList = () => {
-    return items.map((_, idx) => (
-        <CharacterSkeleton key={idx} />
-    ))
-}

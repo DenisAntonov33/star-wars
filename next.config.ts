a479f3a1ca9527f8610babec51f89a51f024a209
DenisAntonov33 <danchic95@gmail.com>
2025-02-17 19:11:31 +0400

    Revert "Feature 1"

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    async redirects() {
-        return [
-            // Basic redirect
-            {
-                source: '/',
-                destination: '/characters',
-                permanent: true,
-            },
-        ]
-    },
+  /* config options here */
 };
 
 export default nextConfig;
--- src/api/http/apiClient.ts
+++
@@ -1,9 +0,0 @@
-export const apiGet = <Response>(input: string | URL, init?: RequestInit): Promise<Response> => {
-    console.debug('GET input >>', input);
-    return fetch(input, init).then(r => r.json());
-}
-
-export const apiPatch = <Response, Payload = {}>(input: string | URL, payload: Payload): Promise<Response> => {
-    console.debug('PATCH payload >>', payload);
-    return Promise.resolve(payload);
-}
